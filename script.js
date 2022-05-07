34d6427ffa8c22ace367694527c058ed1b9a589b
Guilherme Henrique Amaral <101069377+ItalianFanBoy@users.noreply.github.com>
2022-05-06 21:48:45 -0300

    Iniciando projeto de quizz
    finalizando parte de html e css - proximo commit será de js

---
+++ script.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        question: "How old is Guilherme:",
+        a: "10",
+        b: "15",
+        c: "26",
+        d: "110",
+        correct: "c",
+    }, {
+        question: "How old is Rosana?",
+        a:"24",
+        b:"63",
+        c:"34",
+        d: "49",
+        correct: "d",
+
+    }, {
+        question: "Whos the brazilian president?",
+        a: "Lula",
+        b: "Bolsonaro",
+        c: "Ciro", 
+        d: "Tiririca",
+        correct: "b",
+    }, {
+        question: "Quem a mãe ama mais?",
+        a: "Laercio",
+        b: "Guilherme",
+        c: "Gustavo",
+        d: "Todos Nós!!",
+        correct: "a",
+
+    }, {
+        question: "Em que ano a Dona Rosana fez pela ultima vez a rosquinha italiana??",
+        a: "2012",
+        b: "2021",
+        c: "2019",
+        d: "2015",
+        correct: "d"
+        
+    }
+
+]
